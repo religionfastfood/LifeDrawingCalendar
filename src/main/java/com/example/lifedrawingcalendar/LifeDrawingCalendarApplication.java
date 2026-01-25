3652ded92816c4c638195f5610e86e727c13247c
Chris McDonald <christian.mcdonald90@gmail.com>
2026-01-24 18:05:32 -0700

    Initial commit

---
+++ src/main/java/com/example/lifedrawingcalendar/LifeDrawingCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.example.lifedrawingcalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeDrawingCalendarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifeDrawingCalendarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lifedrawingcalendar/LifeDrawingCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lifedrawingcalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeDrawingCalendarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
